0c03f02b048fa065b13294fbdd436a043bbeec63
Zoheb Ansari <zoheb7532@gmail.com>
2025-10-24 12:48:46 +0530

    Move static files to the public directory
    Update the server to serve static files from the new location.

--- user-auth-api/src/server.js
+++ user-auth-api/src/server.js
@@ -26,7 +26,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 
 // Define a simple route for the root URL to confirm the server is running
 app.get("/", (req, res) => {
