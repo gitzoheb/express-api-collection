328c3effa524cfe5fa4052fb140baef5b194e46a
Zoheb Ansari <zoheb7532@gmail.com>
2025-10-24 12:03:03 +0530

    Refactor: Update public directory path
    The public directory path was changed in index.js to 'public'.

--- image-upload-api/src/index.js
+++ image-upload-api/src/index.js
@@ -8,7 +8,7 @@ const PORT = process.env.PORT || 3000;
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('src/public'));
+app.use(express.static('public'));
 
 // Routes
 app.use('/api', uploadRoutes);
